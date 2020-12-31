6601daae8cd3e319df0fc28f55adddd3a89e433c
Huda Khalil <huda_m_khalil@yahoo.com>
2020-12-31 02:41:16 +0000

    fixing npm installation bugs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'karma-designs.eu.auth0.com', // the auth0 domain prefix
+    url: 'karma-designs.eu', // the auth0 domain prefix
     audience: 'CoffeeShop', // the audience set for the auth0 app
     clientId: 'zbzqb1YnjnC6PYKrQmsMufno4vWWlqRx', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
